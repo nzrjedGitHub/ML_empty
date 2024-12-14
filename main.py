e27f2b1f4d0e3689aa50d41d591a4f2f3495bcc7
nzrjedGitHub <98401179+nzrjedGitHub@users.noreply.github.com>
2024-12-14 17:08:14 +0200

    awdas

--- main.py
+++ main.py
@@ -8,3 +8,38 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 df = pd.read_csv('titanic.csv')
 
 print(df.head())
+
+df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'],axis = 1, inplace = True)
+
+df['Embarked'].fillna('S', inplace = True)
+
+print(df.info())
+
+age_1 = df[df['Pclass'] == 1]['Age'].median()
+age_2 = df[df['Pclass'] == 2]['Age'].median()
+age_3 = df[df['Pclass'] == 3]['Age'].median()
+
+def fill_age(row):
+   if pd.isnull(row['Age']):
+       if row['Pclass'] == 1:
+           return age_1
+       if row['Pclass'] == 2:
+           return age_2
+       return age_3
+   return row['Age']
+
+df['Age'] = df.apply(fill_age, axis = 1)
+
+def fill_sex(sex):
+    if sex == 'male':
+        return 1
+    return 0
+
+df['Sex'] = df['Sex'].apply(fill_sex)
+
+print(pd.get_dummies(df['Embarked']))
+
+df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
+df.drop('Embarked', axis = 1, inplace = True)
+
+print(df.head())
